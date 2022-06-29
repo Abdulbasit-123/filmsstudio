ef5e082326c6d9c97f6df093af9bdf25317e8a43
Abdulbasit-123 <bodundeabdulbasit802@gmail.com>
2022-06-29 09:35:19 -0400

    initial commit

---
+++ filmsstudio/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+    <div className="App">
+        <span onClick={() => window.scroll(0,0)} className='header'>🎞️ Film Studio 🎞️</span>
+    </div>
+    );
+}
+
+export default Header;
